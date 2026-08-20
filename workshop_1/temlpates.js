cda3e0ae1db0447ee887e92d519343949dcd7f9e
tavkhelidzeluka <tvakhelidzeluka@gmail.com>
2026-08-20 22:00:24 +0400

    Add workshop 1 code

---
+++ workshop_1/temlpates.js
@@ -0,0 +1,6 @@
+let price = 25;
+let qty = 3;
+let total = price * qty; // computed value
+// template string
+console.log(`Total: ${total} USD`);
+console.log('Total: ${total} USD');
---
+++ workshop_1/variables.js
@@ -0,0 +1,14 @@
+const name = "Giorgi";
+let age = 21;
+
+
+console.log("Hello " + name);
+console.log(age);
+age = age + 1;
+// assignment operator 
+age += 1;
+// ++age;
+// age++;
+age--;
+
+console.log(age);
